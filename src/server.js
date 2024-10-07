08601c7f6bc7c4b656d46b93c05db443f7f5679c
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-10-07 21:59:24 +0300

    add routers

--- src/server.js
+++ src/server.js
@@ -8,7 +8,8 @@ import cors from 'cors';
 // const PORT = Number(process.env.PORT);
 
 import { env } from './utils/env.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+// import { getAllStudents, getStudentById } from './services/students.js';
+import studentsRouter from './routers/students.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -32,35 +33,7 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found students!',
-      data: students,
-    });
-  });
-
-  app.get('/students/:studentId', async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found student with id ${studentId}!`,
-      data: student,
-    });
-  });
+  app.use(studentsRouter);
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
